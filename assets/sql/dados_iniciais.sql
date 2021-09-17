72c82448c12eca3a19cb6bfe4e9e7bfd2801ff0e
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-09-17 22:37:17 +0000

    Preparação da Base de Dados

--- assets/sql/dados_iniciais.sql
+++ assets/sql/dados_iniciais.sql
@@ -8,4 +8,4 @@ INSERT INTO livros(id, titulo, id_genero) VALUES
     (1, 'Assassinato no Expresso do Oriente', 2),
     (2, 'A Sociedade do Anel', 1),
     (3, 'Como eu Era Antes de Você', 3),
-    (4, ' O Guia do MOchileiro das Galáxias', 4);
+    (4, 'O Guia do MOchileiro das Galáxias', 4);
