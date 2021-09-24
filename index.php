d32ad52a6ff12e5fe3a5450f3948da24d9931644
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-09-24 11:15:16 +0000

    Connection Class

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
 <?php
 require_once './vendor/autoload.php';
-use PDO;
+use ExemploPDOMySQL\MySQLConnection; //PDO;
 
-$bd = new PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $bd = new MySQLConnection();
+    $comando = $bd->prepare('INSERT INTO generos(nome) VALUES(:nome)');
+    $comando->execute([':nome' => $_POST['nome']]);
+
+    header('Location:/index.php');
+}
+
+$bd = new MySQLConnection(); //PDO('mysql:host=localhost;dbname=biblioteca', 'root', '');
 
 $comando = $bd->prepare('SELECT *FROM generos');
 $comando->execute();
@@ -17,15 +25,20 @@ $generos = $comando->fetchALL(PDO::FETCH_ASSOC);
     <title>Biblioteca</title>
 </head>
 <body>
+    <a href="insert.php">Novo Gênero</a>
     <table>
         <tr>
             <th>Id</th>
             <th>Nome</th>
+            <th>&nbsp;</th>
     </tr>
     <?php foreach($generos as $g): ?>
         <tr>
             <td><?= $g['id'] ?></td>
             <td><?= $g['nome'] ?></td>   
+            <td>
+                <a href="update.php?id=<?= $g['id'] ?>">Editar</a>
+            </td>
         </tr>
     <?php endforeach ?>
     </table>
