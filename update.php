911b8a4705bd93722538451a1f76341eedd05555
MaFer05 <mariafernanda.05.oliveira@gmail.com>
2021-10-01 11:13:32 +0000

    Bootstrap

--- update.php
+++ update.php
@@ -22,13 +22,21 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     <head>
         <meta charset="UTF-8">
         <title>Novo Gênero</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
     </head>
     <body>
-    <h1>Editar Gênero</h1>
-        <form action="insert.php" method="post">
-            <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
-            <label for="nome">Nome do Gênero</label>
-            <input type="text" require name="nome" value="<?= $genero['id']?>" />
-            <button type="submit">Salvar</button>
-    </form>
+        <main class="container">
+            <h1 style="color: green;">Editar Gênero</h1>
+                <form action="insert.php" method="post">
+                    <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
+                    <div class="form-group">
+                        <label for="nome">Nome do Gênero</label>
+                        <input type="text" require name="nome" value="<?= $genero['id']?>" />
+                    </div><br>
+                    <a class="btn btn-secondary" href="/index.php">Voltar</a>
+                    <button class="btn btn-success" type="submit">Salvar</button>
+                </form>
+        </main>
     </body>
+</html>
